2f901567075af591e3844312a366a613e3ac2aa4
Michael de guzman <michael.deguzman@coreproc.com>
2022-09-22 14:06:30 +0800

    Initial Commit

---
+++ BiometricSampleUITests/BiometricSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BiometricSampleUITestsLaunchTests.swift
+//  BiometricSampleUITests
+//
+//  Created by Michael de Guzman on 9/22/22.
+//
+
+import XCTest
+
+final class BiometricSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
